e358c9310e5eecd46d4d01fe2b6aa2f622299699
nallathambi <bcanallathambi@gmail.com>
2024-12-21 12:09:15 +0530

    Implement web app successfully

--- python/Project/todo_list_project/web_todo.py
+++ python/Project/todo_list_project/web_todo.py
@@ -12,11 +12,13 @@ st.title('Todo App')
 st.subheader('Try using app')
 st.text('this app is developed by python')
 
-for todo in todos:
-	st.checkbox(todo)
-
-
-
+for index, todo in enumerate(todos):
+	checkout = st.checkbox(todo, key=todo)
+	if checkout:
+		todos.pop(index)
+		functions.write_todos(todos)
+		del st.session_state[todo]
+		st.rerun()
 
 st.text_input(label='', placeholder='add a todo list...', on_change=add_todo,
               key='new_todo')
