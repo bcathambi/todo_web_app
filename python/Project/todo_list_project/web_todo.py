438008cb4557ee370dd508c0d35dd498bf97256b
nallathambi <bcanallathambi@gmail.com>
2024-12-21 11:56:20 +0530

    Implement web app for add_todo

---
+++ python/Project/todo_list_project/web_todo.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+	todo = st.session_state['new_todo'] + '\n'
+	todos.append(todo)
+	functions.write_todos(todos)
+
+st.title('Todo App')
+st.subheader('Try using app')
+st.text('this app is developed by python')
+
+for todo in todos:
+	st.checkbox(todo)
+
+
+
+
+st.text_input(label='', placeholder='add a todo list...', on_change=add_todo,
+              key='new_todo')
+
+st.session_state
